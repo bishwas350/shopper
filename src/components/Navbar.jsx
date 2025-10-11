c456b26d26617bdc2c9c685b49bc38ed43e7a3fe
bishwas350 <crashmindboy10@gmail.com>
2025-10-11 22:26:34 +0600

    feat: enhance Navbar functionality and styling; add Poppins font to body

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import Container from './Container'
 import Flex from './Flex'
 import Menu from './Menu'
 import { Button } from './ui/button'
+import { useState } from 'react'
 
 function Navbar() {
+  const [menu,setMenu]=useState("shop")
   return (
     <div className="navbar p-[16px] shadow-[0_1px_3px_-2px_black] ">
       <Container>
@@ -15,17 +17,22 @@ function Navbar() {
         <p className='text-[#171717] font-semibold text-3xl'>BISWAS</p>
       </div>
       <div className="nav-menu">
+        
         <Menu className={'text-xl text-[#626262] font-medium  '}>
-            <li className='hover:text-black cursor-pointer'>Shop</li>
-            <li className='hover:text-black cursor-pointer'>Men</li>
-            <li className='hover:text-black cursor-pointer'>Women</li>
-            <li className='hover:text-black cursor-pointer'>Kids</li>
+            <li onClick={()=>{setMenu("shop")}} className='hover:text-black  cursor-pointer'>Shop{menu==="shop"?<hr className='left-0 -bottom-1 w-full h-[3px] bg-red-500 transition-all duration-500 group-hover:w-full'/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}} className='hover:text-black  cursor-pointer'>Men{menu==="men"?<hr className='left-0 -bottom-1 w-full h-[3px] bg-red-500 transition-all duration-500 group-hover:w-full'/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}} className='hover:text-black  cursor-pointer'>Women{menu==="women"?<hr className='left-0 -bottom-1 w-full h-[3px] bg-red-500 transition-all duration-500 group-hover:w-full'/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}} className='hover:text-black  cursor-pointer'>Kids{menu==="kids"?<hr className='left-0 -bottom-1 w-full h-[3px] bg-red-500 transition-all duration-500 group-hover:w-full'/>:<></>}</li>
         </Menu>
       </div>
       <div className="nav-login-cart gap-4 flex items-center justify-center">
-        <Button variant={'outline'}>login</Button>
-        <button>
+        <Button variant={'outline'} className={'rounded-md cursor-pointer px-9 py-4 active:bg-[#626262]'}>login</Button>
+        <button className='cursor-pointer relative'>
             <ShoppingCart/>
+            
+             <span className="nav-cart-count absolute -top-2 -right-2 bg-[#121212] text-white text-xs font-bold rounded-full h-4 w-4 flex items-center justify-center">
+                      0
+                    </span>
         </button>
       </div>
       </Flex>
