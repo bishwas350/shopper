48ad92b352c14f164e30410aadd8d0b281cc0ba3
bishwas350 <crashmindboy10@gmail.com>
2025-10-13 10:21:38 +0600

    feat: integrate ShopContext and enhance ShopCatagory component with responsive layout and category banners

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -10,8 +8,11 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import LoginSinup from "./pages/LoginSinup";
 import Footer from "./components/Footer";
+import men_banner from './assets/banner_mens.png'
+import women_banner from './assets/banner_women.png'
+import kids_banner from './assets/banner_kids.png'
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
@@ -20,9 +21,9 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
-            <Route path="/men" element={<ShopCatagory catagory="men" />} />
-            <Route path="/women" element={<ShopCatagory catagory="women" />} />
-            <Route path="/kids" element={<ShopCatagory catagory="kids" />} />
+            <Route path="/men" element={<ShopCatagory banner={men_banner} catagory="men" />} />
+            <Route path="/women" element={<ShopCatagory banner={women_banner} catagory="women" />} />
+            <Route path="/kids" element={<ShopCatagory banner={kids_banner} catagory="kids" />} />
             <Route path="/product" element={<Product />}>
               <Route path=":productId" element={<Product />} />
             </Route>
